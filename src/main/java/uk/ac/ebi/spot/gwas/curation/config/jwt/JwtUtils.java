9a0b7ce694127fe50ac9231d57de2bc3b6fbef48
sprintell <abayomi@sprintell.org>
2021-03-18 10:39:19 +0000

    Integrate AAP Authorization Key with jwt

--- src/main/java/uk/ac/ebi/spot/gwas/curation/config/jwt/JwtUtils.java
+++ src/main/java/uk/ac/ebi/spot/gwas/curation/config/jwt/JwtUtils.java
@@ -5,36 +5,67 @@ import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.stereotype.Component;
+import uk.ac.ebi.spot.gwas.curation.config.DepositionCurationConfig;
+
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
+import java.security.PublicKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 @Component
 public class JwtUtils {
 
     private ObjectMapper mapper = new ObjectMapper();
-    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+    private static final Logger log = LoggerFactory.getLogger(JwtUtils.class);
 
-    private String jwtSecret;
+    @Autowired
+    private DepositionCurationConfig depositionCurationConfig;
 
-    private int jwtExpirationMs;
+    private PublicKey verifyingKey;
 
+    @PostConstruct
+    public void initialize() {
+        log.info("Initializing auth cert. Auth enabled: {}", depositionCurationConfig.isAuthEnabled());
+        if (depositionCurationConfig.isAuthEnabled()) {
+            String certPath = depositionCurationConfig.getCertPath();
+            log.info("Using cert: {}", certPath);
+            if (certPath == null) {
+                log.error("Unable to initialize cert. Path is NULL.");
+            } else {
+                try {
+                    InputStream inputStream = new DefaultResourceLoader().getResource(certPath).getInputStream();
+                    final CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+                    final X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(inputStream);
+                    verifyingKey = certificate.getPublicKey();
+                } catch (Exception e) {
+                    log.error("Unable to initialize cert: {}", e.getMessage(), e);
+                }
+            }
+        }
+    }
+    
     public String getUserNameFromJwtToken(String token) {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(verifyingKey).parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateJwtToken(String authToken) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            Jwts.parser().setSigningKey(verifyingKey).parseClaimsJws(authToken);
             return true;
         } catch (SignatureException e) {
-            logger.error("Invalid JWT signature: {}", e.getMessage());
+            log.error("Invalid JWT signature: {}", e.getMessage());
         } catch (MalformedJwtException e) {
-            logger.error("Invalid JWT token: {}", e.getMessage());
+            log.error("Invalid JWT token: {}", e.getMessage());
         } catch (ExpiredJwtException e) {
-            logger.error("JWT token is expired: {}", e.getMessage());
+            log.error("JWT token is expired: {}", e.getMessage());
         } catch (UnsupportedJwtException e) {
-            logger.error("JWT token is unsupported: {}", e.getMessage());
+            log.error("JWT token is unsupported: {}", e.getMessage());
         } catch (IllegalArgumentException e) {
-            logger.error("JWT claims string is empty: {}", e.getMessage());
+            log.error("JWT claims string is empty: {}", e.getMessage());
         }
 
         return false;
