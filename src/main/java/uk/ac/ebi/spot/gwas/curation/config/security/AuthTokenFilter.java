15bf2fcf4070c9b2c2302e1d41a27e62d9d108e9
sprintell <abayomi@sprintell.org>
2021-03-25 17:50:21 +0000

    Customize Spring security for AAP jwt payload

--- src/main/java/uk/ac/ebi/spot/gwas/curation/config/security/AuthTokenFilter.java
+++ src/main/java/uk/ac/ebi/spot/gwas/curation/config/security/AuthTokenFilter.java
@@ -3,7 +3,6 @@ package uk.ac.ebi.spot.gwas.curation.config.security;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -21,7 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
 
@@ -49,20 +46,17 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
-
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
             return headerAuth.substring(7, headerAuth.length());
         }
-
-        return "";
+        return null;
     }
 
     public UsernamePasswordAuthenticationToken getAuthFromJwtToken(Claims claims) {
-        Map<String, Object> authData = mapper.convertValue(claims.get("authentication"), new TypeReference<Map<String, Object>>(){});
-        List<String> roles = mapper.convertValue(claims.get("role"), new TypeReference<List<String>>(){});
+        AapPayload aapPayload = mapper.convertValue(claims, new TypeReference<AapPayload>(){});
+        List<String> roles = aapPayload.convertAapDomainsToSpringSecurityRoles();
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         roles.forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
-        return new UsernamePasswordAuthenticationToken(
-                authData.get("principal"), authData.get("credentials"), authorities);
+        return new UsernamePasswordAuthenticationToken("", null, authorities);
     }
 }
