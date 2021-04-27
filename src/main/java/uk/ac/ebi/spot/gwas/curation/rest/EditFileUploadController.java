41fb99d9425c34d026adc3788bb9cacb31443497
Sajo John <sajo@ebi.ac.uk>
2021-04-27 11:37:44 +0100

    GOCI-3 Curator Edit Template Lock Feature

--- src/main/java/uk/ac/ebi/spot/gwas/curation/rest/EditFileUploadController.java
+++ src/main/java/uk/ac/ebi/spot/gwas/curation/rest/EditFileUploadController.java
@@ -3,42 +3,28 @@ package uk.ac.ebi.spot.gwas.curation.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.Resources;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import uk.ac.ebi.spot.gwas.curation.config.RestInteractionConfig;
 import uk.ac.ebi.spot.gwas.curation.constants.DepositionCurationConstants;
-import uk.ac.ebi.spot.gwas.curation.constants.IDPConstants;
 import uk.ac.ebi.spot.gwas.curation.service.EditFileUploadService;
 import uk.ac.ebi.spot.gwas.curation.service.JWTService;
 import uk.ac.ebi.spot.gwas.curation.service.SubmissionService;
 import uk.ac.ebi.spot.gwas.curation.service.UserService;
-import uk.ac.ebi.spot.gwas.curation.service.impl.EditFileUploadServiceImpl;
 import uk.ac.ebi.spot.gwas.curation.util.CurationUtil;
-import uk.ac.ebi.spot.gwas.curation.util.HeadersUtil;
 import uk.ac.ebi.spot.gwas.deposition.constants.GeneralCommon;
-import uk.ac.ebi.spot.gwas.deposition.domain.Submission;
-import uk.ac.ebi.spot.gwas.deposition.domain.User;
 import uk.ac.ebi.spot.gwas.deposition.dto.FileUploadDto;
 import uk.ac.ebi.spot.gwas.deposition.dto.SubmissionDto;
 import uk.ac.ebi.spot.gwas.deposition.rest.RestRequestUtil;
-
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping(value = GeneralCommon.API_V1 + DepositionCurationConstants.API_SUBMISSIONS)
@@ -79,16 +65,17 @@ public class EditFileUploadController {
     @ResponseBody
     public Resource<FileUploadDto> uploadEditFile(@RequestParam MultipartFile file,
                                                   @PathVariable String submissionId,
-                                                  HttpServletRequest request) throws IOException {
+                                                  HttpServletRequest request)  {
 
         String jwtToken = CurationUtil.parseJwt(request);
-        log.info("Jwt token ->"+jwtToken);
         ResponseEntity<Resource<FileUploadDto>> fileUploadDtoResource = editFileUploadService.uploadEditFIle(jwtToken, submissionId, file );
 
        return fileUploadDtoResource.getBody();
     }
 
-
+    /*
+     * PUT /v1/submissions/{submissionId}/lock?lockStatus=lock|unlock
+     */
     @PutMapping(value = "/{submissionId}" + DepositionCurationConstants.API_SUBMISSIONS_LOCK,
             produces = MediaTypes.HAL_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
@@ -96,7 +83,6 @@ public class EditFileUploadController {
 
 
         String jwtToken = CurationUtil.parseJwt(request);
-        log.info("Jwt token ->"+jwtToken);
         ResponseEntity<Resource<SubmissionDto>> submissionDtoResource = editFileUploadService.lockSubmission(jwtToken, lockStatus , submissionId );
         return submissionDtoResource.getBody();
 
