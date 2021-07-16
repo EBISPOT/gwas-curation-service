8f3e414c081c197f573c71e8ff5d6d5cf75828f3
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2021-07-16 12:26:12 +0100

    gwas-curation-service#6 Handling invalid sumstats submission error

--- src/main/java/uk/ac/ebi/spot/gwas/curation/service/ConversionJaversService.java
+++ src/main/java/uk/ac/ebi/spot/gwas/curation/service/ConversionJaversService.java
@@ -12,6 +12,11 @@ public interface ConversionJaversService {
 
     public Optional<Map<Double, List<JaversChangeWrapper>>> filterJaversResponse(List<JaversChangeWrapper> javersChangeWrapperList);
 
+    public List<Double> removeInvalidSumstatsEntries(List<JaversChangeWrapper> javersChangeWrapperList);
+
+    public void removeVersionMap(Optional<Map<Double, List<JaversChangeWrapper>>> javersChangeWrapperMap,
+                                 List<Double> versionsTobeRemoved );
+
     public Optional<List<FileUpload>> filterJaversResponseForFiles(List<JaversChangeWrapper> javersChangeWrapperList);
 
     public List<VersionSummary> filterStudiesFromJavers(Optional<Map<Double, List<JaversChangeWrapper>>> javersChangeWrapperList);
