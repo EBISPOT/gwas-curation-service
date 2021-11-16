f9085ec64b80d9bdc34f2b2f71b3461d60d86ef4
ala <63405719+ala-ebi@users.noreply.github.com>
2021-11-16 16:55:35 +0000

    efo traits bulk upload, delete, update

--- src/main/java/uk/ac/ebi/spot/gwas/curation/repository/StudyRepository.java
+++ src/main/java/uk/ac/ebi/spot/gwas/curation/repository/StudyRepository.java
@@ -1,8 +1,5 @@
 package uk.ac.ebi.spot.gwas.curation.repository;
 
-import org.javers.spring.annotation.JaversSpringDataAuditable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import uk.ac.ebi.spot.gwas.deposition.domain.Study;
 
@@ -14,4 +11,5 @@ public interface StudyRepository extends MongoRepository<Study, String> {
 
     Stream<Study> readByIdIn(List<String> ids);
 
+    Stream<Study> findByEfoTraitListContains(String traitId);
 }
