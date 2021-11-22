2f5fecff18fd40a75b489e9d7c1f5bee6efe3b29
ala <63405719+ala-ebi@users.noreply.github.com>
2021-11-22 15:35:46 +0000

    resolve further conflicts

--- src/main/java/uk/ac/ebi/spot/gwas/curation/service/impl/EfoTraitServiceImpl.java
+++ src/main/java/uk/ac/ebi/spot/gwas/curation/service/impl/EfoTraitServiceImpl.java
@@ -60,10 +60,10 @@ public class EfoTraitServiceImpl implements EfoTraitService {
         efoTraits.forEach(efoTrait -> {
             try {
                 createEfoTrait(efoTrait, user);
-                report.add(new TraitUploadReport(efoTrait.getTrait(),"Trait successfully Inserted : " + efoTrait.getTrait()));
+                report.add(new TraitUploadReport(efoTrait.getTrait(),"Trait successfully Inserted : " + efoTrait.getTrait(), null));
             }
             catch(DataAccessException ex){
-                report.add(new TraitUploadReport(efoTrait.getTrait(),"Trait Insertion failed as Trait already exists : " + efoTrait.getTrait()));
+                report.add(new TraitUploadReport(efoTrait.getTrait(),"Trait Insertion failed as Trait already exists : " + efoTrait.getTrait(), null));
             }
         });
         return report;
