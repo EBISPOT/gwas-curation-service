71e0f67d6cb0223379baa740c0c887574db057cd
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2022-10-07 11:03:48 +0100

    curation#139 Rabbit MQ Prod related Config changes

--- src/main/java/uk/ac/ebi/spot/gwas/curation/config/MyRabbitConfiguration.java
+++ src/main/java/uk/ac/ebi/spot/gwas/curation/config/MyRabbitConfiguration.java
@@ -7,30 +7,31 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.ac.ebi.spot.gwas.curation.constants.DepositionCurationConstants;
+import uk.ac.ebi.spot.gwas.deposition.config.RabbitMQConfigProperties;
 
 @Configuration
 public class MyRabbitConfiguration {
 
-    String QUEUE_NAME = DepositionCurationConstants.QUEUE_NAME;
-    String EXCHANGE_NAME = DepositionCurationConstants.EXCHANGE_NAME;
-    String ROUTING_KEY = DepositionCurationConstants.ROUTING_KEY;
+    @Autowired
+    RabbitMQConfigProperties rabbitMQConfigProperties;
 
     @Bean
     Queue queue(){
-        return new Queue(QUEUE_NAME, true);
+        return new Queue(rabbitMQConfigProperties.getQueueName(), true);
     }
 
     @Bean
     DirectExchange exchange(){
-        return new DirectExchange(EXCHANGE_NAME);
+        return new DirectExchange(rabbitMQConfigProperties.getExchangeName());
     }
 
     @Bean
     Binding binding(Queue queue, DirectExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+        return BindingBuilder.bind(queue).to(exchange).with(rabbitMQConfigProperties.getRoutingKey());
     }
 
 
