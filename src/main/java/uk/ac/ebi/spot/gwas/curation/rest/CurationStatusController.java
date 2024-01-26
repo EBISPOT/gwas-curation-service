58147fb39c5e977db9c87ab73a55cd91bb8f53dd
ala <63405719+ala-ebi@users.noreply.github.com>
2024-01-26 14:46:34 +0000

    fix merge

--- src/main/java/uk/ac/ebi/spot/gwas/curation/rest/CurationStatusController.java
+++ src/main/java/uk/ac/ebi/spot/gwas/curation/rest/CurationStatusController.java
@@ -38,9 +38,7 @@ public class CurationStatusController {
     @PreAuthorize("hasRole('self.GWAS_Curator')")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
-    @PreAuthorize("hasRole('self.GWAS_Curator')")
-    public PagedResources<CurationStatusDTO> getAllCurationStatus(PagedResourcesAssembler assembler,
-                                                               @PageableDefault(size = 10, page = 0) Pageable pageable) {
+    public PagedResources<CurationStatusDTO> getAllCurationStatus(PagedResourcesAssembler assembler, @PageableDefault(size = 10, page = 0) Pageable pageable) {
        Page<CurationStatus> curationStatuses = curationStatusService.findAllCurationStatus(pageable);
         final ControllerLinkBuilder lb = ControllerLinkBuilder.linkTo(ControllerLinkBuilder
                 .methodOn(CurationStatusController.class).getAllCurationStatus(assembler, pageable));
@@ -53,7 +51,6 @@ public class CurationStatusController {
     @PreAuthorize("hasRole('self.GWAS_Curator')")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{curatorStatusId}")
-    @PreAuthorize("hasRole('self.GWAS_Curator')")
     public Resource<CurationStatusDTO> getCurationStatus(@PathVariable String curatorStatusId) {
         CurationStatus curationStatus = curationStatusService.findCurationStatus(curatorStatusId);
         if(curationStatus != null){
