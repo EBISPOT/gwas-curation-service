0e84b04100c3b70f88b1a88826842dd75f978196
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2024-05-07 14:39:52 +0100

    goci-1276 Sync Pmid Oracle Code review Comments

--- src/main/java/uk/ac/ebi/spot/gwas/curation/rabbitmq/MetadataYmlUpdateConsumer.java
+++ src/main/java/uk/ac/ebi/spot/gwas/curation/rabbitmq/MetadataYmlUpdateConsumer.java
@@ -1,9 +1,10 @@
-/*
+
 package uk.ac.ebi.spot.gwas.curation.rabbitmq;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.spot.gwas.curation.constants.DepositionCurationConstants;
 import uk.ac.ebi.spot.gwas.deposition.dto.curation.MetadataYmlUpdate;
@@ -15,16 +16,12 @@ metadata Yaml hence we don't need to have this consumer
 **/
 
 
-/*
 
+@Profile({"local"})
 @Component
 public class MetadataYmlUpdateConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(MetadataYmlUpdateConsumer.class);
-
-
-
-
 
     @RabbitListener(queues = {DepositionCurationConstants.QUEUE_NAME_SUMSTATS_SANDBOX} )
     public void listen(MetadataYmlUpdate metadataYmlUpdate) {
@@ -37,4 +34,4 @@ public class MetadataYmlUpdateConsumer {
     }
 }
 
-*/
+
